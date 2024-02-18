60a98125a99dd67f277647732fc4f65387c984c4
Brandon Ly <brandonkly@ucla.edu>
2024-02-18 15:19:41 -0800

    progress

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,10 @@
+import Sidebar from "../components/Sidebar";
+
 export default function HomePage() {
+
+
+
   return <>
-    <div className="text-2xl bg-red-800 w-12 h-12">Hello World</div>
-  </>;
+    <Sidebar />
+  </>
 }
